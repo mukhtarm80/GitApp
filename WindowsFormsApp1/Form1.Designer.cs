64b11142d8404e1eba7c06b8f14c211efd922234
mukhtarm80 <41829599+mukhtarm80@users.noreply.github.com>
2018-07-28 21:59:53 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(58, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 255);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<Phone> phones = new List<Phone>
+            {
+                new Phone { Id = 11, Name = "Samsung Galaxy Ace 2", Year = 2012},
+                new Phone { Id = 12, Name = "Samsung Galaxy S4", Year = 2013},
+                new Phone { Id = 13, Name = "iPhone 6", Year = 2014},
+                new Phone { Id = 14, Name = "Microsoft Lumia 435", Year = 2015},
+                new Phone { Id = 15, Name = "Xiaomi Mi 5", Year = 2015}
+            };
+            listBox1.DataSource = phones;
+            listBox1.DisplayMember = "Name";
+            listBox1.ValueMember = "Id";
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+
+        }
+        void listBox1_SelectedIndexChanged(object sender,EventArgs e) 
+        {
+            int id = (int)listBox1.SelectedValue;
+
+            Phone phone = (Phone)listBox1.SelectedItem;
+            MessageBox.Show(id.ToString() + ". " + phone.Name);
+        }
+    }
+    class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+    }
+}
